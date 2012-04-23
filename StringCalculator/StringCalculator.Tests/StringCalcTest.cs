c15c00d83ae928d6d6d1474be2fa1e26408deef7
caspencer <cas628@gmail.com>
2012-04-23 11:17:37 -0400

    initial commit of String Calculator kata

---
+++ StringCalculator/StringCalculator.Tests/StringCalcTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalcTest
+    {
+        [Fact]
+        public void Add_ShouldReturn0ForEmptyString()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(0, calc.Add(string.Empty));
+        }
+
+        [Fact]
+        public void Add_ShouldReturn1For_1()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(1, calc.Add("1"));
+        }
+
+        [Fact]
+        public void Add_ShouldReturn3For_1_2()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(3, calc.Add("1,2"));
+        }
+
+        [Fact]
+        public void Add_ShouldReturn3For_1_newline_2()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(3, calc.Add("1\n2"));
+        }
+
+        [Fact]
+        public void Add_ShouldSupportChangingDelimiter()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(3, calc.Add("//;\n1;2")); // "//[delimiter]\n[numbers]"
+        }
+
+        [Fact]
+        public void Add_ShouldThrowExceptionOnNegative()
+        {
+            var calc = new StringCalc();
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => calc.Add("-1"));
+            Assert.Equal("negatives not allowed: -1\r\nParameter name: input", exception.Message);            
+        }
+
+        [Fact]
+        public void Add_ShouldThrowExceptionOnMultipleNegatives()
+        {
+            var calc = new StringCalc();
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => calc.Add("-1,-2"));
+            Assert.Equal("negatives not allowed: -1,-2\r\nParameter name: input", exception.Message);
+        }
+
+        [Fact]
+        public void Add_ShouldIgnoreNumbersGreaterThan1000()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(2, calc.Add("2,1001"));
+        }
+
+        [Fact]
+        public void Add_ShouldSupportMultiCharacterDelimiters()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(6, calc.Add("//[***]\n1***2***3")); // "//[delimiter]\n[numbers]"
+        }
+
+        [Fact]
+        public void Add_ShouldSupportMultipleDelimiters()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(6, calc.Add("//[*][%]\n1*2%3")); // "//[delim1][delim2]\n[numbers]"
+        }
+
+        [Fact]
+        public void Add_ShouldSupportMultipleMultiCharacterDelimiters()
+        {
+            var calc = new StringCalc();
+            Assert.Equal(6, calc.Add("//[***][%%%]\n1***2%%%3")); // "//[delim1][delim2]\n[numbers]"
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator/StringCalc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class StringCalc
+    {
+        public int Add(string input)
+        {
+            int sum = 0;
+            List<string> delimiters = new List<string>(new string[] { ",", "\n" });
+            List<int> negativeNumbers = new List<int>();
+
+            if (input == string.Empty)
+                return sum;
+
+            // override delimiter
+            if (input.StartsWith("//"))
+            {
+                int indexOfFirstNewline = input.IndexOf('\n');
+                string delimiterLine = input.Substring(2, indexOfFirstNewline-2);
+
+                // is multichar delimiter?
+                if (delimiterLine.StartsWith("[") && delimiterLine.EndsWith("]"))
+                {
+                    delimiterLine = delimiterLine.Substring(1, delimiterLine.LastIndexOf(']') - 1);
+                    delimiters.AddRange(delimiterLine.Split(new[] { "][" }, StringSplitOptions.None));
+                }
+                else
+                {
+                    delimiters.Add(delimiterLine);
+                }
+
+                input = input.Substring(indexOfFirstNewline + 1);
+            }
+
+            var numbers = input.Split(delimiters.ToArray(), StringSplitOptions.None);
+            foreach (var number in numbers)
+            {
+                int numberVal = Convert.ToInt32(number);
+                if (numberVal < 0)
+                    negativeNumbers.Add(numberVal);
+
+                if (numberVal <= 1000)
+                    sum += numberVal;
+            }
+
+            if (negativeNumbers.Count > 0)
+            {
+                string msg = string.Format("negatives not allowed: {0}", string.Join(",", negativeNumbers.ToArray()));
+                throw new ArgumentOutOfRangeException("input", msg);
+            }
+
+            return sum;
+        }
+    }
+}
