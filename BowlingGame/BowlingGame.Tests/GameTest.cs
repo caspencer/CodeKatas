f0482e8648533bcb1892f0385a445377846e4c1d
caspencer <cas628@gmail.com>
2012-04-24 09:53:59 -0400

    initial add of BowlingGame, started adding a couple simple tests

---
+++ BowlingGame/BowlingGame.Tests/GameTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace BowlingGame.Tests
+{
+    public class GameTest
+    {
+        [Fact]
+        public void Score_ShouldReturn0IfRollNeverCalled()
+        {
+            var game = new Game();
+            Assert.Equal(0, game.Score());
+        }
+
+        [Fact]
+        public void Score_ShouldReturn0IfRolledCalledWith0Pins()
+        {
+            var game = new Game();
+            game.Roll(0);
+            Assert.Equal(0, game.Score());
+        }
+
+        [Fact]
+        public void Score_ShouldReturn1IfRolledCalledWith1Pins()
+        {
+            var game = new Game();
+            game.Roll(1);
+            Assert.Equal(1, game.Score());
+        }
+    }
+}
---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int lastPins;
+
+        public void Roll(int pins)
+        {
+            lastPins = pins;
+        }
+
+        public int Score()
+        {
+            return lastPins;
+        }
+    }
+}
